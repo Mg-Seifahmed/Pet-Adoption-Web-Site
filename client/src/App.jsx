12275075be428d0c6b5372f7ffecd234d1a9f004
Mohamed Tarek Mohamed <angrym21@gmail.com>
2024-05-16 21:27:23 +0300

    Pet listing is done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,34 +1,35 @@
 import './App.css'
-import {BrowserRouter,Routes,Route} from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './Components/Navbar/Navbar'
 import Homepage from './Pages/Homepage';
 import AnimalsPage from './Pages/Animalspage';
 import Pet from './Pages/Pet';
 import Login from './Components/Logincomp/Login';
 import Signup from './Components/SignupComp/Signup';
+import PetListing from './Components/PetAddcomp/PetListing';
 
 
 function App() {
-  
+
 
   return (
     <>
-    <BrowserRouter>
-      <Navbar/>
+      <BrowserRouter>
+        <Navbar />
 
-      <Routes>
-      
-      <Route path='/' element={<Homepage />}/>
-          <Route path='/dogs' element={<AnimalsPage category="dogs"/>}/>
-          <Route path='/cats' element={<AnimalsPage category="cats"/>}/>
+        <Routes>
+
+          <Route path='/' element={<Homepage />} />
+          <Route path='/dogs' element={<AnimalsPage category="dogs" />} />
+          <Route path='/cats' element={<AnimalsPage category="cats" />} />
           <Route path="/pet" element={<Pet />}>
             <Route path=":petid" element={<Pet />} />
           </Route>
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
-        
-      </Routes>
-    </BrowserRouter>
+          <Route path="/Addpet" element={<PetListing />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
--- client/src/Components/Navbar/Navbar.jsx
+++ client/src/Components/Navbar/Navbar.jsx
@@ -6,15 +6,11 @@ import { Link } from 'react-router-dom';
 const Navbar = () => {
   return (
     <nav>
-        <div>
-          <h1>PetFindEgy</h1>
-        </div>
+      <div>
+        <h1>PetFindEgy</h1>
+      </div>
       <ul class="menu">
         <li><a>Adopt or get involved</a></li>
-        <ul class="submenu">
-            <li><a>About PetFindEgy</a></li>
-            <li><a>Animal Shelters & Rescues</a></li>
-        </ul>
         <li>
           <a>Dogs And Puppies</a>
           <ul class="submenu">
@@ -23,16 +19,16 @@ const Navbar = () => {
           </ul>
         </li>
         <li><a>Cats and Kittens</a>
-            <ul class="submenu">
-                <li><a>Cat Adoption</a></li>
-                <li><a>Cat Info</a></li>
-            </ul>
+          <ul class="submenu">
+            <li><a>Cat Adoption</a></li>
+            <li><a>Cat Info</a></li>
+          </ul>
         </li>
-        
+
       </ul>
       <div>
-      <Link to="/signup"> <button><span>Signup</span></button></Link>
-      <Link to="/login"> <button><span>Login</span></button></Link>
+        <Link to="/signup"> <button className='navbarbutton'><span>Signup</span></button></Link>
+        <Link to="/login"> <button className='navbarbutton'><span>Login</span></button></Link>
       </div>
     </nav>
   )
