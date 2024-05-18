937c1d4dc2f7a628aaa8133f86486a0751880297
Abdulrahman-Allam <abdulrahman.allam.cs@gmail.com>
2024-05-18 04:09:19 +0300

    saving 2

---
+++ server/model/Pet_Request.js
@@ -0,0 +1,58 @@
+import mongoose from "mongoose";
+
+const petRequestSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    characteristics: {
+        type: String,
+        required: true
+    },
+    health: {
+        type: String,
+        required: true
+    },
+    color: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    adult: {
+        type: Boolean,
+        required: true
+    },
+    houseTrained: {
+        type: Boolean,
+        required: true
+    },
+    spayedNeutered: {
+        type: Boolean,
+        required: true
+    },
+    gender: {
+        type: String,
+        required: true
+    },
+    img: {
+        type: String,
+        required: true
+    },
+    breed: {
+        type: String,
+        required: true
+    },
+    publisher: {
+        type: String,
+    },
+    pEmail: {
+        type: String,
+    }
+})
+
+const Pet_Request = mongoose.model("Pet_Request", petRequestSchema);
+export default Pet_Request;
+
